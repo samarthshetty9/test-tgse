21e3245f5a57d39d8eb8f6f60091881d8376b186
samarthshetty9 <sammyshetty09@gmail.com>
2026-01-16 13:37:08 +0530

    Remove scroll indicator from Hero section

--- src/components/home/Hero.js
+++ src/components/home/Hero.js
@@ -63,12 +63,7 @@ const Hero = () => {
         </div>
       </div>
 
-      {/* Scroll Indicator - Reverted to Absolute but Centered */}
-      <div className="absolute bottom-8 left-0 w-full flex justify-center animate-bounce opacity-70 cursor-pointer text-white z-20">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7" />
-        </svg>
-      </div>
+
     </section>
   );
 };
